85520ef0f2145af74a55cb10352547114e8c0831
Nikita Yuzva <yuzva71@mail.ru>
2023-01-30 03:58:18 +0200

    PyTest test_abs

---
+++ test_abs.py
@@ -0,0 +1,31 @@
+def test_abs1():
+    assert abs(-42) == 42, "Should be absolute value of a number"
+
+def test_abs2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
+#import pytest
+#
+#from selenium import webdriver
+#from selenium.webdriver.common.by import By
+#from selenium.common.exceptions import NoSuchElementException
+#
+#
+#def test_exception1():
+#    try:
+#        browser = webdriver.Chrome()
+#        browser.get("http://selenium1py.pythonanywhere.com/")
+#        with pytest.raises(NoSuchElementException):
+#            browser.find_element(By.CSS_SELECTOR, "button.btn")
+#            pytest.fail("Не должно быть кнопки Отправить")
+#    finally:
+#        browser.quit()
+#
+#def test_exception2():
+#    try:
+#        browser = webdriver.Chrome()
+#        browser.get("http://selenium1py.pythonanywhere.com/")
+#        with pytest.raises(NoSuchElementException):
+#            browser.find_element(By.CSS_SELECTOR, "no_such_button.btn")
+#            pytest.fail("Не должно быть кнопки Отправить")
+#    finally:
+#        browser.quit()
